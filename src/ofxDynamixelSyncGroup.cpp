5af22e99f6358c0e9478c368da533daa6e8d637e
Roy Macdonald <macdonald.roy@gmail.com>
2019-02-27 22:52:11 -0300

    added sync group class

---
+++ src/ofxDynamixelSyncGroup.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxDynamixelSyncGroup.cpp
+//  test_autoDistribucion
+//
+//  Created by Roy Macdonald on 2/27/19.
+//
+//
+
+#include <stdio.h>
---
+++ src/ofxDynamixelSyncGroup.h
@@ -0,0 +1,13 @@
+//
+//  ofxDynamixelSyncGroup.h
+//  test_autoDistribucion
+//
+//  Created by Roy Macdonald on 2/27/19.
+//
+//
+
+#ifndef ofxDynamixelSyncGroup_h
+#define ofxDynamixelSyncGroup_h
+
+
+#endif /* ofxDynamixelSyncGroup_h */
