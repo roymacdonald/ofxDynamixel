6a0ebdf38e14a71f30404049565d6068ae9a8c18
Roy Macdonald <macdonald.roy@gmail.com>
2019-03-03 21:24:09 -0300

    updated gui groups

--- src/ofxDynamixelGuiGroups.h
+++ src/ofxDynamixelGuiGroups.h
@@ -1,13 +1,29 @@
 //
 //  ofxDynamixelGuiGroups.h
-//  example_multiServo
+
 //
 //  Created by Roy Macdonald on 3/3/19.
 //
 //
 
-#ifndef ofxDynamixelGuiGroups_h
-#define ofxDynamixelGuiGroups_h
+#pragma once
+#include <string>
 
-
-#endif /* ofxDynamixelGuiGroups_h */
+namespace ofxDynamixel {
+namespace GuiGroups {	
+static const std::string info              = "info";
+static const std::string settings_comm     = "settings/comm";
+static const std::string settings_mode     = "settings/mode";
+static const std::string dynamics          = "dynamics";
+static const std::string settings_limits   = "settings/limits";
+static const std::string settings_info     = "settings/info";
+static const std::string settings_status   = "settings/status";
+static const std::string status            = "status";
+static const std::string settings_PID      = "settings/PID";
+static const std::string dynamics_pwm      = "dynamics/pwm";
+static const std::string dynamics_velocity = "dynamics/velocity";
+static const std::string dynamics_position = "dynamics/position";
+static const std::string dynamics_profile  = "dynamics/profile";
+	
+}
+}
